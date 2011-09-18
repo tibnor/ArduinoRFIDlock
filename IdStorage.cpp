087f629d276421adff62c4f0ea4d16a9df017f9f
Paul Levold <paal.levold@gmail.com>
2011-09-18 14:01:37 +0200

    Changed more 12s to ID_SIZE

--- IdStorage.cpp
+++ IdStorage.cpp
@@ -1,4 +1,5 @@
 #include "IdStorage.h"
+//#include <EPROM.h>
 
 
 IdStorage::IdStorage() {
@@ -7,13 +8,13 @@ IdStorage::IdStorage() {
   idAdmin[0] = 0x00;
 }
 
-boolean IdStorage::storeId(byte id[12]) {
+boolean IdStorage::storeId(byte id[ID_SIZE]) {
   //Check if storage is full
   if (idPos >= 19) return false;
 
   //Check if there is an admin, if not store it
   if (idAdmin[0] == 0x00) {
-    for (int i = 0; i < 12; i = i + 1){
+    for (int i = 0; i < ID_SIZE; i = i + 1){
       idAdmin[i] = id[i];
     }
     return true;
@@ -22,7 +23,7 @@ boolean IdStorage::storeId(byte id[12]) {
   if(typeOfUser(id)==UNKNOWN){
 
     //All Ok, store id
-    for (int i = 0; i < 12; i = i + 1){
+    for (int i = 0; i < ID_SIZE; i = i + 1){
       ids[idPos][i] = id[i];
     }
     idPos = idPos + 1;    
@@ -35,7 +36,7 @@ boolean IdStorage::storeId(byte id[12]) {
 
 void IdStorage::printIds() {
   Serial.print("Admin: ");
-  for (int j = 0; j < 12; j = j + 1) {
+  for (int j = 0; j < ID_SIZE; j = j + 1) {
     Serial.print(idAdmin[j],BYTE);
   }
   Serial.println();
@@ -45,7 +46,7 @@ void IdStorage::printIds() {
     Serial.print("Id");
     Serial.print(i);
     Serial.print(": ");
-    for (int j = 0; j < 12; j = j + 1) {
+    for (int j = 0; j < ID_SIZE; j = j + 1) {
       Serial.print(ids[i][j],BYTE);
     }
     Serial.println(", ");
@@ -53,7 +54,7 @@ void IdStorage::printIds() {
   Serial.println("Done");
 }
 
-boolean IdStorage::TagMatch(byte sFirst[12],byte sSecond[12])
+boolean IdStorage::TagMatch(byte sFirst[ID_SIZE],byte sSecond[ID_SIZE])
 { 
   for (byte bTmp=1; bTmp < 11; bTmp++)
   {
@@ -68,7 +69,7 @@ boolean IdStorage::TagMatch(byte sFirst[12],byte sSecond[12])
 #define UNKNOWN 0
 #define USER 1
 #define ADMIN 2
-byte IdStorage::typeOfUser(byte tag[12]) {
+byte IdStorage::typeOfUser(byte tag[ID_SIZE]) {
   for (byte i = 0; i<idPos;i++){
     if(TagMatch(ids[i],tag))
       return USER;
@@ -85,7 +86,7 @@ void IdStorage::loadEPROM() {
 }
 
 void IdStorage::storeEPROM() {  
-  
+
 }
 
 
