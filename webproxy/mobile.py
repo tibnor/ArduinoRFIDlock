a8a60502d8764c2156abae58d6e2b8834b5d0d32
Torstein Ingebrigtsen Bø <torsteinibo@gmail.com>
2011-11-25 23:17:18 +0100

    Added word and key in mobile

--- webproxy/mobile.py
+++ webproxy/mobile.py
@@ -5,39 +5,73 @@ import random
 import hashlib
 from password import Password
 
-class MainPage(webapp.RequestHandler):
+
+class Mobile(db.Model):
+    Key = db.StringProperty()
+    word = db.IntegerProperty()
+    
+    def updateKey(self):  
+        key = random.randint(0,4611686018427387904)      
+        m = hashlib.sha1(str(Password.mobileServerPass)+str(key));
+        self.Key = m.hexdigest()    
+        self.put()  
+        return key  
+    
+    def getKey(self):
+        return self.Key
+    
+    def updateWord(self):
+        word = random.randint(0,4611686018427387904)      
+        self.word = word
+        self.put()
+        return word
+    
+    def getWord(self):
+        return self.word
+    
+class RequestKey(webapp.RequestHandler):
     def get(self):
-        id = self.request.get("id")
         self.response.headers['Content-Type'] = 'text/plain'
-        mobile = Mobile.get_or_insert(id)
+        mobile = Mobile()
         key = mobile.updateKey()
-        self.response.out.write(key)
+        id = mobile.key().id()
+        self.response.out.write("{\"status\":200,\"id\":"+str(id)+",\"word\":"+str(key)+"}")
+    
+class RequestWord(webapp.RequestHandler):
+    def get(self):
+        id = int(self.request.get("id"))
+        self.response.headers['Content-Type'] = 'text/plain'
+        mobile = Mobile.get_by_id(id)
+        if (mobile is None):
+            self.response.out.write("{\"status\":400}")
+        else:
+            word = mobile.updateWord()
+            self.response.out.write("{\"status\":200,\"word\":"+str(word)+"}")
+            
 
 class ToggleDoor(webapp.RequestHandler):
     def get(self):
-        id = self.request.get("id")
+        id = int(self.request.get("id"))
         hashInput = self.request.get("hash")
-        mobile = Mobile.get_by_key_name(id)
+        mobile = Mobile.get_by_id(id)
         self.response.headers['Content-Type'] = 'text/plain'
         #self.response.out.write("input hashInput:"+str(hashInput)+"\n") 
         self.response.out.write("{")
         if (mobile is None):
             self.response.out.write("\"status\":400")
         else:
-            #self.response.out.write("Key:"+str(mobile.getKey())+"\n")
-            m = hashlib.sha1( str(mobile.getKey()+Password.mobileServerPass));
+            m = hashlib.sha1( str(mobile.getKey())+str(mobile.getWord()));
             hashServer = m.hexdigest()          
             #self.response.out.write("Server hashServer:"+str(hashServer))
             if (hashInput == hashServer):
-                #self.response.out.write("\nTrue")
-                self.response.out.write("\"status\":200,\"key\":"+str(mobile.updateKey()))
+                self.response.out.write("\"status\":200,\"word\":"+str(mobile.updateWord()))
             else:
                 self.response.out.write("\"status\":401")
                 
         self.response.out.write("}")
         
 
-application = webapp.WSGIApplication([('/requestkey', MainPage),('/toggledoor',ToggleDoor)], debug=True)
+application = webapp.WSGIApplication([('/requestkey',RequestKey),('/requestword', RequestWord),('/toggledoor',ToggleDoor)], debug=True)
 
 
 def main():
@@ -46,14 +80,4 @@ def main():
 if __name__ == "__main__":
     main()
     
-class Mobile(db.Model):
-    Key = db.IntegerProperty()
-    
-    def updateKey(self):  
-        key = random.randint(0,4611686018427387904)      
-        self.Key = key
-        self.put()
-        return key
-    
-    def getKey(self):
-        return self.Key
+
